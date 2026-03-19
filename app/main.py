d2fade03807416336b91d67a42bef4acad6f30b1
thechronic04 <dasb4@trendly.ai>
2026-03-20 00:44:01 +0530

    cleanup: remove accessories from db and seed script

--- app/main.py
+++ app/main.py
@@ -25,6 +25,7 @@ try:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=settings.ALLOWED_CORS_ORIGINS,
+        allow_origin_regex="https://.*\.vercel\.app",
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
